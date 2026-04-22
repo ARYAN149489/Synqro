1f39dbcc14c00fd526ee23d24a768a96b002b4a8
Aryan Kansal <aryankansal@Aryans-MacBook-Air-2.local>
2026-04-22 21:11:59 +0530

    Fix syntax errors in UserModel crashing server

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,47 +1,49 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
 
-        lowercase: true,
-        unique:true,
-        index:true
+      lowercase: true,
+      unique: true,
+      index: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
-    }
-}, {
-    timestamps: true
-});
-
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // hash pass
-userSchema.pre('save', async function(next){
-    if(!this,this.isModified('password')){
-        return next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 // compare pass
-userSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcrypt.matchPassword(enteredPassword, this.password);
-}
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-const user = mongoose.model('User', userSchema);
+const user = mongoose.model("User", userSchema);
 module.exports = user;
